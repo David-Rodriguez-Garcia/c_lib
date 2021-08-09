1909bdab050b4ad9a1f43a36467b1447ada13c3f
David Rodriguez garcia <davirodr@c3r7s1.42madrid.com>
2021-08-09 16:48:28 +0200

    Removed files

--- plantillas/ft_strlen.c
+++
@@ -1,9 +0,0 @@
-//#include "../libft.a"
-#include <stdio.h>
-unsigned long	ft_strlen(const char *s);
-
-int main(void)
-{
-	printf("%ld", ft_strlen("Esta es una frase de 29 chars"));
-	return(0);
-}
